6923d670cdd38696859bc90cff4cedc85cfaf588
Trsenli <ztruth@qq.com>
2021-05-05 21:22:48 +0800

    --story=1007168 --user=lzl token传输 https://www.tapd.cn/39901720/s/1015083

--- src/main/java/com/oj/ojbackend/pojo/Problem.java
+++ src/main/java/com/oj/ojbackend/pojo/Problem.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Problem {
     private String pb_id;
-
     private String pb_file_id;
     private String pb_title;
     private String pb_description;
